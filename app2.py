50cfbe2d8ceb4fbdfe4d831fbfafd1ab5bd15131
Vinicius <vinicius.wu.93@gmail.com>
2025-08-17 18:07:40 -0700

    third try

---
+++ app2.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import yfinance as yf
+import streamlit as st
+
+st.title("Streamlit Test Page")
+st.write("✅ If you see this text, Streamlit is working fine!")
+st.line_chart([1, 5, 2, 6, 3, 7])
